cd20258b370778abb691ee0877714652ca92208b
fujie33 <2990989544@qq.com>
2020-10-18 18:24:05 +0800

    Java Simple Pojo Upload

---
+++ JavaBasic/src/main/java/com/fujie/learn/week1/Example3_10.java
@@ -0,0 +1,24 @@
+package com.fujie.learn.week1;
+
+/**
+ * @ClassName Example3_10
+ * @Description TODO
+ * @Author 付洁
+ * @Date 2020/10/18
+ **/
+class XiyouRenwu{
+    float height,weight;
+    String head,ear;
+    void speak(String s){
+        System.out.println(s);
+    }
+}
+
+
+public class Example3_10 {
+    public static void main(String args[]){
+        XiyouRenwu zhubajie,sunwukong;
+        zhubajie = new XiyouRenwu();
+        sunwukong = new XiyouRenwu();
+    }
+}
